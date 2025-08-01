d4af5b060c1592b4bea7184c2d604e2e11efcebd
Pablo Treo <pablotreo@gmail.com>
2025-08-01 07:43:51 -0300

    actualizaciones varias, footer, slider and lenis

---
+++ src/utils/lenis.js
@@ -0,0 +1,17 @@
+// src/utils/lenis.js
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis({
+  smooth: true,
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+export default lenis
